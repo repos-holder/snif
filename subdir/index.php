5fcf480b6a4deea1312f2a44429f87c486df2bf7
A. V. Lukyanov <artem.falcon@gmail.com>
2014-08-31 14:41:20 +0400

    own fork of this crap

--- subdir/index.php
+++ subdir/index.php
@@ -2,15 +2,10 @@
 /***************************************************************************
 
 snif 1.2.1
-"snif is not an index file"
-"simple and nice index file"
 (c) Kai Blankenhorn
 www.bitfolge.de
-kaib@bitfolge.de
-
 
 THIS IS THE SUBDIR/INDEX.PHP FILE.
-
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -23,24 +18,10 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details: <http://www.gnu.org/licenses/gpl.txt>
 
 ****************************************************************************
-
-
-Changelog:
-
-see the real snif index file
-
-
-****************************************************************************
 **  THIS FILE IS SUPPOSED TO SHOW A DIRECTORY LISTING??                   **
 ****************************************************************************
 
-No, this file is used as a forwarder to the main snif file. See the example
-in the real snif index file.
-
-
-
-****************************************************************************/
-
+No, this file is used as a forwarder to the main snif file.*/
 
 $dir = dirname($_SERVER["PHP_SELF"]);
 $pathArr = explode("/",$dir);
@@ -52,5 +33,5 @@ if ($path[0]=='/')
 
 $subDirectory = $pathArr[count($pathArr)-1]."/".$path;
 
-Header("Location: ../?path=".$subDirectory);
+Header("Location: ../path/".$subDirectory);
 ?>
